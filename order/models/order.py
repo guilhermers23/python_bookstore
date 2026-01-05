2fad3393ca2538b2ebf22d187858e3650d8d2b0a
Guilherme Rosa <guilhermemuralha2016@gmail.com>
2026-01-05 20:38:35 -0300

    14.4 - Migrando Django Models

--- order/models/order.py
+++ order/models/order.py
@@ -3,5 +3,5 @@ from product.models.product import Product
 from django.contrib.auth.models import User
 
 class Order(models.Model):
-    product = models.ManyToManyField(Product, blank=False);
-    user = models.ForeignKey(User, Null=False)
+    product = models.ManyToManyField(Product, blank=False)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
