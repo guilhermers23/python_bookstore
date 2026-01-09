c316fc930a9e9c642ccbd8ea73a62ee29f292a60
Guilherme Rosa <guilhermemuralha2016@gmail.com>
2026-01-09 19:43:08 -0300

    17.4 - ViewSets e Autenticação

--- bookstore_python/settings.py
+++ bookstore_python/settings.py
@@ -126,9 +126,8 @@ INTERNAL_IPS = [ '127.0.0.1', 'localhost']
 REST_FRAMEWORK = {
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE': 10,  # ou outro número de sua preferência
-    
-    'DEFAULT_AUTHENTICATION_CLASSES': [
+     'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.BasicAuthentication',
         'rest_framework.authentication.SessionAuthentication',
-        'rest_framework_jwt.authentication.JSONWebTokenAuthentication',
     ]
 }
